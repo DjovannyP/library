91c49ca57a9186e5f35fa20364701a830d489479
DjovannyP <pierredjovanny77@gmail.com>
2024-02-23 16:03:27 -0400

    Form event listener updated

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const newBook = document.querySelector('button')
-const container = document.querySelector('.container')
+// Selecting DOM elements
+const newBook = document.querySelector('#add_new_book');
+const container = document.querySelector('.container');
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+const submitButton = document.querySelector('button[type="submit"]');
+const myLibrary = [];
 
-const myLibrary = ["je saurais", "I am a good programmer", "Que du tort"];
-
-
+// Defining the Book class to represent books
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read || false;
-
     this.info = function info() {
         if (this.read) {
             return `The ${this.title} by ${this.author}, ${this.pages} pages, read.`;
@@ -19,15 +21,39 @@ function Book(title, author, pages, read) {
     };
 }
 
+// Add or remove the 'hidden' class from the container when clicking the 'newBook' button
 newBook.addEventListener('click', () => {
-    container
-})
-//take input to user
+    container.classList.toggle('hidden');
+});
+
 function addBookToLibrary() {
-
-    let input = prompt("Enter a book");
-
-    myLibrary.push(input);
+    // Create an instance of Book with values from the form
+    const book = new Book(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value);
+    myLibrary.push(book);
 }
 
-myLibrary.forEach((book) => console.log(book))
+function displayLibrary() {
+    const bookGrid = document.querySelector('.right');
+console.log(bookGrid);
+    bookGrid.innerHTML = '';
+
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        const bookInfo = document.createElement('p');
+        bookInfo.textContent = book.info();
+        bookCard.appendChild(bookInfo);
+
+        bookGrid.appendChild(bookCard);
+    });
+}
+
+
+// Event listener for the form submission
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    addBookToLibrary(); 
+    container.classList.toggle('hidden');
+    bookDisplay();
+});

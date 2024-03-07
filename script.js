deb23203be456bc38cc0ab01b5c9d6945d9557f5
DjovannyP <pierredjovanny77@gmail.com>
2024-03-07 10:30:50 -0400

    Finalize project: Added styling for book cards

--- script.js
+++ script.js
@@ -11,49 +11,67 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read || false;
-    this.info = function info() {
-        if (this.read) {
-            return `The ${this.title} by ${this.author}, ${this.pages} pages, read.`;
-        } else {
-            return `The ${this.title} by ${this.author}, ${this.pages} pages, not read yet.`;
-        }
-    };
+    this.read = read;
 }
+//read
+Book.prototype.toggleRead = function () {;
+    this.read = !this.read;
+};
 
+function toggleRead(index) {;
+    myLibrary[index].toggleRead();
+    render();
+};
 // Add or remove the 'hidden' class from the container when clicking the 'newBook' button
 newBook.addEventListener('click', () => {
     container.classList.toggle('hidden');
+    newBook.textContent = newBook.textContent === 'NEW BOOK' ? 'CLOSE' : 'NEW BOOK';
 });
 
 function addBookToLibrary() {
     // Create an instance of Book with values from the form
-    const book = new Book(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value);
+    const book = new Book(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].checked);
     myLibrary.push(book);
+    render();
+    
 }
 
-function displayLibrary() {
-    const bookGrid = document.querySelector('.right');
-console.log(bookGrid);
+//display book in myLibrary
+function render() {
+    let bookGrid = document.querySelector('.right');
     bookGrid.innerHTML = '';
-
-    myLibrary.forEach((book) => {
+    for (let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];//each book in the array
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
-
-        const bookInfo = document.createElement('p');
-        bookInfo.textContent = book.info();
-        bookCard.appendChild(bookInfo);
-
-        bookGrid.appendChild(bookCard);
-    });
+        bookCard.innerHTML = `<p>- Title: ${book.title}</p><hr>
+                     <p>- Written by: ${book.author}</p><hr>
+                     <p>- Number of pages: ${book.pages}</p><hr>
+                     <p>- Status: ${book.read ? "Read" : "Not Read Yet"}.</p><hr>
+                     <div class="icon">
+                        <i class="ri-delete-bin-7-line" onclick="deleteBook(${i})"> Remove</i>
+                        <i class="ri-toggle-line" onclick="toggleRead(${i})"> Reading Status</i>
+                     </div>`;
+         bookGrid.appendChild(bookCard);
+        
+    }
+   
 }
 
-
 // Event listener for the form submission
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     addBookToLibrary(); 
     container.classList.toggle('hidden');
-    bookDisplay();
+    form.reset()
+    newBook.textContent = 'NEW BOOK';
 });
+
+//Delete book from array
+const deleteBook = (index) => {
+    myLibrary.splice(index, 1);
+    render()
+
+
+}
+console.log(myLibrary);
